2b3e345a65ce79b47d8496f2f35f4486ca1d05a0
Nodespond <neapasdesmon@mail.ru>
2024-01-17 05:18:34 +1000

    first commit

---
+++ SimpleInherit.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+class Human {
+    protected:
+
+    public:
+
+};
+
+
+
+
+
+int main()
+{
+    Human human1;
+    Human human2;
+    Human human3;
+    
+    
+    return 0;
+}
+
